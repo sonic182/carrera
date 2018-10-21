7e8197f5fb7dbea81a2cb1b9dc9c09fcd5786296
sonic182 <johander1822@gmail.com>
2018-10-21 14:10:46 +0200

    First commit

---
+++ run.py
@@ -0,0 +1,4 @@
+
+from carrera import main
+
+main()
---
+++ setup.py
@@ -0,0 +1,37 @@
+"""Setup module."""
+
+from setuptools import setup
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
+
+
+def requirements(filename):
+    """Parse requirements from requirements.txt."""
+    reqs = parse_requirements(filename, session=False)
+    return [str(req.req) for req in reqs]
+
+
+setup(
+    name='carrera',
+    version='0.0.1',
+    description='Concurrency Framework',
+    author='Johanderson Mogollon',
+    author_email='johanderson@mogollon.com.ve',
+    license='MIT',
+    packages=['carrera'],
+    setup_requires=['pytest-runner'],
+    test_requires=['pytest'],
+    install_requires=requirements('requirements.txt'),
+    extras_require={
+        'dev': requirements('dev-requirements.txt'),
+        'test': [
+            'pytest',
+            'pytest-cov',
+            'pytest-sugar',
+            'coverage',
+            'coveralls'
+        ]
+    }
+)
