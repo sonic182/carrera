63c690b4cf99e90e58fc5968983e83cb6083302f
sonic182 <johander1822@gmail.com>
2018-10-31 23:42:21 +0100

    Fix test on travis

--- carrera/dispatcher.py
+++ carrera/dispatcher.py
@@ -15,7 +15,7 @@ class Dispatcher(object):
 
     def __new__(cls, *args, **kwargs):
         if not cls.instance:
-            cls.instance = object.__new__(cls, *args, **kwargs)
+            cls.instance = object.__new__(cls)
         return cls.instance
 
     def __init__(self):
